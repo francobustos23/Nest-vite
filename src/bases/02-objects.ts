a46ee1c3eed62a609c3ecf31532f96b17f5c8504
francobustos23 <bustosfranco071@gmail.com>
2024-05-14 15:26:41 -0300

    Objects Pk

--- src/bases/02-objects.ts
+++ src/bases/02-objects.ts
@@ -1,11 +1,23 @@
-export const pokemonsIds = [1, 20, 30, 34, 66]
+export const pokemonsIds = [1, 20, 30, 34, 66];
 
-export const pokemon = {
-    id:1,
+
+interface Pokemon {
+    id: number;
+    name: string;
+    age?: number;
+}
+
+export const bulbasaur: Pokemon = {
+    id: 1,
     name: "Bulbasaur",
 }
 
-console.log(pokemon);
+export const charmander: Pokemon = {
+    id: 2,
+    name: "Charmander"
+}
+
+console.log(bulbasaur);
 
 // pokemonsIds.push(+'1') //el + convierte el string a number
 
